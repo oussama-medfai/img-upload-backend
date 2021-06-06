561a33ecd67bc1fcc3460087ac536a5fb348aaf6
oussama-medfai <oussama-medfai>
2021-06-06 12:32:50 +0100

    init deploy 2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const webRoutes = require('./routes/web');
+const webRoutes = require("./routes/web");
 
 app.use(cors());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 // Front Routes handling
 app.use(webRoutes);
